19cc60cb440d5decc6671823c33fd6e8efbbc095
svahabi <42755892+svahabi@users.noreply.github.com>
2018-08-29 16:35:20 -0700

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,35 @@
+public class Arithmetic
+{
+	public static void main (String args[])
+	{
+		int a = 5;
+		int b = 10;
+		float x = 15;
+		float y = 20;
+		float  sum1 = a + b;
+		float sum2 = a + x;
+		float sum3 = a + y;
+		float sum4 = b + x;
+		float sum5 = b + y;
+
+		float div1 = a / b;
+		float div2 = a / x;
+		float div3 = a / y;
+		float div4 = b / x;
+		float div5 = b / y;
+		System.out.println(sum1);
+		System.out.println(sum2);
+		System.out.println(sum3);
+		System.out.println(sum4);
+		System.out.println(sum5);
+
+		System.out.println(div1);
+		System.out.println(div2);
+		System.out.println(div3);
+		System.out.println(div4);
+		System.out.println(div5);
+
+		
+	
+	}
+}
